7f749e9a4cd48ca06c12db1be9dd345eef39ec2e
Michael Davitt <63263218+michaeldavitt@users.noreply.github.com>
2022-01-25 10:22:43 +0000

    Removed readme and added main code and setup

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name="os_info",
+    description="A package allowing you to view information about your machine",
+    version="1.0.0",
+    scripts=["sys_info_comp30830"]
+)
